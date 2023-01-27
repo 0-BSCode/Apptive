9487c9b0f2fd0a1c2452ed5c891c2d148d7363d1
Bryan Sanchez <bryanipsanchez@gmail.com>
2023-01-27 18:43:21 +0800

    Commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "480px",
+      md: "512px",
+      lg: "768px",
+      xl: "1024px",
+      "2xl": "1280px",
+      "3xl": "1440px",
+    },
     extend: {},
   },
   plugins: [],
